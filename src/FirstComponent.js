5ee6fdc0515ee0f3ca291d1d4f515c5a40a82e0a
Piotr Lewandowski <piotrl@Piotrs-MacBook-Pro.local>
2022-02-10 13:21:32 -0500

    created our first working react application

---
+++ src/FirstComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'; // good practice to import (ES6 way; require is the ES5 way)
+
+function FirstComponent() {
+    return (
+        <h1>This is cool. 😄</h1>
+    )
+}
+
+
+export default FirstComponent; // can be imported somewhere else now
